370dd9035a06c981348a5206bb17b97f1c16c0e3
McBallins <warier@live.com>
2022-01-02 20:02:36 -0600

    adds psuedo code for game board functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+const gameboard = (() => {
+  we have 9 lactions
+  each location has three possible states- x, o, null
+  each location can be an Object
+  we take 2 inputs, location on the board and player(x or o)
+  check to see if the location is available
+  store an object that saves the new state of the location
+  write x or o into the corresponding location
+  when three xs or os are place in a row, one player wins
+  if the board is full and there is not a row of xs or os, tie
+  const check = console.log('gameboard accesible');
+  return 'check';
+})();
