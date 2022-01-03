b7e2eacc5d5907cdacf34fdd4c28f62007028b18
McBallins <warier@live.com>
2022-01-02 22:44:35 -0600

    changed Gameboard from an object to a method

--- script.js
+++ script.js
@@ -1,42 +1,29 @@
-const Gameboard = {
-  positions: [
+const Gameboard = (() => {
+  const positions = [
     ,
-  ],
+  ]
 
-  position: (function() {
+  const postionPiece = (() => {
     const positions = document.querySelectorAll('.column');
     positions.forEach(position => {
       position.addEventListener('click', event => {
         console.log('player.choice', position.dataset.location);
-        Gameboard.placeInPosition('player.choice', position.dataset.location);
+        placeInPosition('player.choice', position.dataset.location);
       })
     })
-  })(),
+  })();
 
-  placeInPosition: function(symbol, selectedPositon) {
-    if(Gameboard.positions[selectedPositon] === undefined) {
-    Gameboard.positions[selectedPositon] = symbol;
+  const placeInPosition = function(symbol, selectedPositon) {
+    if(positions[selectedPositon] === undefined) {
+    positions[selectedPositon] = symbol;
     console.log(symbol + ' has been placed on ' + selectedPositon);
-    console.log((`[data-location=’${selectedPositon}’]`));
     const placement = document.querySelector(`[data-location="${selectedPositon}"]`);
-    console.log(placement);
     placement.textContent = symbol;
     } else {
       console.log(`There is a ${this.positions[selectedPositon]} already there!` );
     }
-  },
-  // we have 9 lactations
-  // each position has three possible states- x, o, null
-  // each position can be an Object
-  // we take 2 inputs, position on the board and player(x or o)
-  // check to see if the position is available
-  // store an object that saves the new state of the position
-  // write x or o into the corresponding position
-  // when three xs or os are place in a row, one player wins
-  // if the board is full and there is not a row of xs or os, tie
-  // const check = console.log('gameboard accesible');
-  // return 'check';
-}
+  }
+})();
 
 const Game = {
   checkEndofGame: function () {
