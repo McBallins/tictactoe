b8dd53d814dc9a6d4d7d28165d8df17d605bc931
McBallins <warier@live.com>
2022-01-03 18:13:09 -0600

    form data gets taken in now

--- script.js
+++ script.js
@@ -1,44 +1,3 @@
-const Gameboard = (() => {
-  let positions = [];
-
-  const addListeners = (() => {
-    const squares = document.querySelectorAll('.column');
-    squares.forEach(square => {
-      square.addEventListener('click', event => {
-        const location = square.dataset.location;
-        placeInPosition(location);
-      })
-    })
-  })();
-
-
-  const placeInPosition = function(location) {
-
-    let symbol;
-    console.log(Players.playerOne.isCurrentPlayer)
-    if(Players.playerOne.isCurrentPlayer) {
-      choice = Players.playerOne.choice
-    } else {
-      choice = Players.playerTwo.choice
-    }
-    if(positions[location] === undefined) {
-    positions[location] = choice;
-    console.log(choice + ' has been placed on ' + location);
-    const position = document.querySelector(`[data-location="${location}"]`);
-    position.textContent = choice;
-    Game.checkEndofGame();
-    Game.switchTurn();
-    } else {
-      console.log(`There is a ${this.positions[location]} already there!` );
-    };
-  };
-
-  return {
-    placeInPosition: placeInPosition,
-    positions : positions
-  };
-})();
-
 const Game = (() => {
 
   const switchTurn = function() {
@@ -54,6 +13,8 @@ const Game = (() => {
     }
   }
 
+
+    //this is broken
     const checkEndofGame = () => {
       console.log('checking end of game');
       if(Players.playerOne.choice === Gameboard.positions[0] && Players.playerOne.choice === Gameboard.positions[1] && Players.playerOne.choice === Gameboard.positions[2] || 
@@ -93,16 +54,35 @@ const Players = (function() {
 
   function Player(choice) {
     this.choice = choice;
-    this.isCurrentPlayer = true;
+    this.isCurrentPlayer = '';
   }
   
-  const playerOne = new Player('x');
-  const playerTwo = new Player('o');
-  playerTwo.isCurrentPlayer = false;
+  const playerOne = new Player('');
+  const playerTwo = new Player('');
+
+  const getUserData = () => {
+    const playerOneName = document.getElementById('playeronename');
+    const playerTwoName = document.getElementById('playertwoname');
+    xAssignment = document.getElementById('radioplayerone');
+    playerOne.name = playerOneName.value;
+    playerTwo.name = playerTwoName.value;
+    if(xAssignment.value === 'Player One') {
+      playerOne.choice = 'x';
+      playerTwo.choice = 'o'
+      playerOne.isCurrentPlayer = true;
+      playerTwo.isCurrentPlayer = false;
+    } else {
+      playerOne.choice = 'o';
+      playerTwo.choice = 'x'
+      playerOne.isCurrentPlayer = false;
+      playerTwo.isCurrentPlayer = true;
+    }
+  }
 
   return {
     playerOne: playerOne,
-    playerTwo: playerTwo
+    playerTwo: playerTwo,
+    getUserData: getUserData,
   }
 
 })();
@@ -112,11 +92,12 @@ const InfoBoard = (() => {
 
   const makeForms = (() => {
     const myForms = [
-      ['TEXT', 'playerOneName', 'INPUT'],
-      ['TEXT', 'playerTwoName', 'INPUT'],
+      ['TEXT', 'playeronename', 'INPUT', ''],
+      ['TEXT', 'playertwoname', 'INPUT', ''],
       ['', '', 'P', 'Which Player is Going first?'],
-      ['RADIO', 'radioPlayerOne', 'INPUT', 'Palyer One'],
-      ['RADIO', 'radioPlayerOne', 'INPUT', 'Player Two'],
+      ['RADIO', 'radioplayerone', 'INPUT', 'Player One'],
+      ['RADIO', 'radioplayertwo', 'INPUT', 'Player Two'],
+      ['', 'submit', 'BUTTON', 'Begin Game'],
     ]
     let i = 0;
     myForms.forEach((form => {
@@ -126,6 +107,7 @@ const InfoBoard = (() => {
       newForm.type = myForms[i][0];
       newForm.id = myForms[i][1];
       newForm.name = myForms[i][1];
+      newForm.value = myForms[i][3];
       newForm.textContent = myForms[i][3];
       if(myForms[i][0] === 'RADIO') {
         console.log('works')
@@ -148,4 +130,49 @@ const InfoBoard = (() => {
 
 const aiPlayer = (() => {
 
+})();
+
+const Gameboard = (() => {
+  let positions = [];
+
+  const addListeners = (() => {
+    const squares = document.querySelectorAll('.column');
+    squares.forEach(square => {
+      square.addEventListener('click', event => {
+        const location = square.dataset.location;
+        placeInPosition(location);
+      })
+    })
+    const submitButton = document.getElementById('submit');
+    submitButton.addEventListener('click', event =>{
+      Players.getUserData();
+    })
+  })();
+
+
+  const placeInPosition = function(location) {
+
+    let symbol;
+    console.log(Players.playerOne.isCurrentPlayer)
+    if(Players.playerOne.isCurrentPlayer) {
+      choice = Players.playerOne.choice
+    } else {
+      choice = Players.playerTwo.choice
+    }
+    if(positions[location] === undefined) {
+    positions[location] = choice;
+    console.log(choice + ' has been placed on ' + location);
+    const position = document.querySelector(`[data-location="${location}"]`);
+    position.textContent = choice;
+    Game.checkEndofGame();
+    Game.switchTurn();
+    } else {
+      console.log(`There is a ${this.positions[location]} already there!` );
+    };
+  };
+
+  return {
+    placeInPosition: placeInPosition,
+    positions : positions
+  };
 })();
