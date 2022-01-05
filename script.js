aa154b9917e6bde75268dfb2fd0a5bc7a4cc9c17
McBallins <warier@live.com>
2022-01-04 18:36:50 -0600

    adding a new game button broke so much i don't even know, but it's fixed now, and there's a reset button

--- script.js
+++ script.js
@@ -12,48 +12,19 @@ const Game = (() => {
     }
   }
 
-  function gameState(boolean) {
-    this.gameOver = boolean;
-  }
-
-    const gameOver = new gameState(true);
-
-    const checkEndofGame = () => {
-      console.log('checking...')
-      if(Players.playerOne.choice === Gameboard.positions[0] && Players.playerOne.choice === Gameboard.positions[1] && Players.playerOne.choice === Gameboard.positions[2] || 
-        Players.playerOne.choice === Gameboard.positions[0] && Players.playerOne.choice === Gameboard.positions[4] && Players.playerOne.choice === Gameboard.positions[8] || 
-        Players.playerOne.choice === Gameboard.positions[0] && Players.playerOne.choice === Gameboard.positions[3] && Players.playerOne.choice === Gameboard.positions[6] || 
-        Players.playerOne.choice === Gameboard.positions[1] && Players.playerOne.choice === Gameboard.positions[4] && Players.playerOne.choice === Gameboard.positions[7] || 
-        Players.playerOne.choice === Gameboard.positions[2] && Players.playerOne.choice === Gameboard.positions[4] && Players.playerOne.choice === Gameboard.positions[6] || 
-        Players.playerOne.choice === Gameboard.positions[2] && Players.playerOne.choice === Gameboard.positions[5] && Players.playerOne.choice === Gameboard.positions[8] || 
-        Players.playerOne.choice === Gameboard.positions[3] && Players.playerOne.choice === Gameboard.positions[4] && Players.playerOne.choice === Gameboard.positions[5] || 
-        Players.playerOne.choice === Gameboard.positions[6] && Players.playerOne.choice === Gameboard.positions[7] && Players.playerOne.choice === Gameboard.positions[8]) {
-        Players.playerOne.winner = true;
-        gameOver.gameOver = true;
-      }
-      else if(Gameboard.positions[0] !== undefined && Gameboard.positions[1] !== undefined && Gameboard.positions[2] !== undefined && Gameboard.positions[3] !== undefined 
-        && Gameboard.positions[5] !== undefined && Gameboard.positions[6] !== undefined && Gameboard.positions[7] !== undefined && Gameboard.positions[8] !== undefined) {
-          gameOver.gameOver = true;
-      } else if(Players.playerTwo.choice === Gameboard.positions[0] && Players.playerTwo.choice === Gameboard.positions[1] && Players.playerTwo.choice === Gameboard.positions[2] || 
-        Players.playerTwo.choice === Gameboard.positions[0] && Players.playerTwo.choice === Gameboard.positions[4] && Players.playerTwo.choice === Gameboard.positions[8] || 
-        Players.playerTwo.choice === Gameboard.positions[0] && Players.playerTwo.choice === Gameboard.positions[3] && Players.playerTwo.choice === Gameboard.positions[6] || 
-        Players.playerTwo.choice === Gameboard.positions[1] && Players.playerTwo.choice === Gameboard.positions[4] && Players.playerTwo.choice === Gameboard.positions[7] || 
-        Players.playerTwo.choice === Gameboard.positions[2] && Players.playerTwo.choice === Gameboard.positions[4] && Players.playerTwo.choice === Gameboard.positions[6] || 
-        Players.playerTwo.choice === Gameboard.positions[2] && Players.playerTwo.choice === Gameboard.positions[5] && Players.playerTwo.choice === Gameboard.positions[8] || 
-        Players.playerTwo.choice === Gameboard.positions[3] && Players.playerTwo.choice === Gameboard.positions[4] && Players.playerTwo.choice === Gameboard.positions[5] || 
-        Players.playerTwo.choice === Gameboard.positions[6] && Players.playerTwo.choice === Gameboard.positions[7] && Players.playerTwo.choice === Gameboard.positions[8]) {
-        Players.playerTwo.winner = true;
-        gameOver.gameOver = true;
-      }
-
-      if(gameOver.gameOver) {
-        endGame();
-        console.log(gameOver.gameOver)
-      } else {
-        switchTurn();
-        console.log(gameOver.gameOver)
-      }
+  const gameOver = (() => {
+    let gameOver = true
+    const set = (boolean) => {
+      gameOver = boolean;
     }
+    const getGameOver = () => {
+      return gameOver;
+    }
+    return {
+      set : set,
+      getGameOver : getGameOver,
+    }
+  })();
 
     const endGame = function() {
       if(Players.playerOne.winner === true) {
@@ -63,13 +34,17 @@ const Game = (() => {
       } else {
         InfoBoard.print('The game ends in a tie!', true);
       };
-      gameOver.gameOver = true;
-      console.log(gameOver.gameOver);
+      Game.gameOver.set(true);
     }
+
+    const init = (() => {
+      // gameOver();
+    })();
     
     return {
-      checkEndofGame: checkEndofGame,
+      endGame: endGame,
       gameOver : gameOver,
+      switchTurn : switchTurn,
       }
 
 })();
@@ -176,14 +151,24 @@ const InfoBoard = (() => {
     placeResetButton();
   }
 
+  const addSubmitListener = () => {
+    const submitButton = document.getElementById('submit');
+    submitButton.addEventListener('click', event =>{
+      Players.getUserData();
+      deleteForms();
+      Game.gameOver.set(false);
+    })
+  };
+
   const reset = () => {
     while (infoBoard.lastChild) {
       infoBoard.removeChild(infoBoard.lastChild);
     }
     Gameboard.reset();
     makeForms();
+    addSubmitListener();
     Players.reset();
-    Game.gameOver.gameOver = true;
+    Game.gameOver.set(true);
   }
 
   const placeResetButton = () => {
@@ -198,6 +183,7 @@ const InfoBoard = (() => {
 
   const init = (() => {
     makeForms();
+    addSubmitListener();
   })();
 
   return {
@@ -211,13 +197,86 @@ const aiPlayer = (() => {
 })();
 
 const Gameboard = (() => {
-  let positions = [];
+
+  const myPositions = ((position, symbol) => {
+    let a1 = '';
+    let a2 = '';
+    let a3 = '';
+    let b1 = '';
+    let b2 = '';
+    let b3 = '';
+    let c1 = '';
+    let c2 = '';
+    let c3 = '';
+
+    const convertPosition = (position, choice) => {
+      position = position*1;
+      if(position === 0 && a1 !== 'o'&& a1 !== 'x') {
+        myPositions.a1 = choice;
+        console.log(myPositions.a1)
+        place(position, choice);
+      } else if(position === 1 && a2 !== 'o'&& a2 !== 'x') {
+        myPositions.a2 = choice;
+        place(position, choice);
+      } else if(position === 2 && a3 !== 'o'&& a3 !== 'x') {
+        myPositions.a3 = choice;
+        place(position, choice);
+      } else if(position === 3 && b1 !== 'o'&& b1 !== 'x') {
+        myPositions.b1 = choice;
+        place(position, choice);
+      } else if(position === 4 && b2 !== 'o'&& b2 !== 'x') {
+        myPositions.b2 = choice;
+        place(position, choice);
+      } else if(position === 5 && b3 !== 'o'&& b3 !== 'x') {
+        myPositions.b3 = choice;
+        place(position, choice);
+      } else if(position === 6 && c1 !== 'o'&& c1 !== 'x') {
+        myPositions.c1 = choice;
+        place(position, choice);
+      } else if(position === 7 && c2 !== 'o'&& c2 !== 'x') {
+        myPositions.c2 = choice;
+        place(position, choice);
+      } else if(position === 8 && c3 !== 'o'&& c3 !== 'x') {
+        myPositions.c3 = choice;
+        place(position, choice);
+      } else {
+        InfoBoard.print(`There is already a ${choice.toUpperCase()} there!`);
+      }
+    };
+
+    const getPos = (pos) => {
+      console.log(myPositions,pos)
+      newPos = myPositions.pos;
+      return newPos;
+      }
+
+    const reset = () => {
+      myPositions.a1 = '';
+      myPositions.a2 = '';
+      myPositions.a3 = '';
+      myPositions.b1 = '';
+      myPositions.b2 = '';
+      myPositions.b3 = '';
+      myPositions.c1 = '';
+      myPositions.c2 = '';
+      myPositions.c3 = '';
+    }
+
+    return {
+      convertPosition : convertPosition,
+      a1 : a1,
+      getPos : getPos,
+      reset : reset
+    }
+  })();
 
   const squares = document.querySelectorAll('.column');
   const addSquareListeners = () => {
     squares.forEach(square => {
+      console.log('sqaure listeners placed')
       square.addEventListener('click', event => {
-        if(Game.gameOver.gameOver === false) {
+        console.log(Game.gameOver.getGameOver())
+        if(Game.gameOver.getGameOver() === false) {
         const location = square.dataset.location;
         placeInPosition(location);
         }
@@ -225,56 +284,81 @@ const Gameboard = (() => {
     })
   };
 
-  const addSubmitListener = () => {
-    const submitButton = document.getElementById('submit');
-    submitButton.addEventListener('click', event =>{
-      Players.getUserData();
-      InfoBoard.deleteForms();
-      Game.gameOver.gameOver = false;
-      console.log(Game.gameOver.gameOver)
-    })
-  };
-
   const placeInPosition = function(location) {
-    if(1 === 1) {
+    if(Game.gameOver.getGameOver() === false) {
+      let choice;
       if(Players.playerOne.isCurrentPlayer) {
         choice = Players.playerOne.choice
       } else {
         choice = Players.playerTwo.choice
       }
-      if(positions[location] === undefined) {
-        positions[location] = choice;
-        console.log(choice + ' has been placed on ' + location);
-        const position = document.querySelector(`[data-location="${location}"]`);
-        position.textContent = choice;
-        Game.checkEndofGame();
-        console.log(Game.gameOver.gameOver);
-      } else {
-        InfoBoard.print(`There is already a ${positions[location].toUpperCase()} there!`)
-      };
-  } else {
-    InfoBoard.print(`You need start a game first!`);
+      myPositions.convertPosition(location, choice);
+    } else {
+      InfoBoard.print(`You need start a game first!`);
+    }
   }
-}
+  const place = (position, choice) => {
+    console.log(position)
+    const boardPosition = document.querySelector(`[data-location="${position}"]`);
+    console.log(boardPosition)
+    boardPosition.textContent = choice;
+    checkEndofGame();
+    };
+
+    const checkEndofGame = () => {
+      console.log('checking...')
+      console.log(myPositions.getPos('a1'))
+      if(Players.playerOne.choice === myPositions.a1 && Players.playerOne.choice === myPositions.a2 && Players.playerOne.choice === myPositions.a3 || 
+        Players.playerOne.choice === myPositions.a1 && Players.playerOne.choice === myPositions.b2 && Players.playerOne.choice === myPositions.c3 || 
+        Players.playerOne.choice === myPositions.a1 && Players.playerOne.choice === myPositions.b1 && Players.playerOne.choice === myPositions.c1 || 
+        Players.playerOne.choice === myPositions.a2 && Players.playerOne.choice === myPositions.b2 && Players.playerOne.choice === myPositions.c2 || 
+        Players.playerOne.choice === myPositions.a3 && Players.playerOne.choice === myPositions.b2 && Players.playerOne.choice === myPositions.c1 || 
+        Players.playerOne.choice === myPositions.a3 && Players.playerOne.choice === myPositions.b3 && Players.playerOne.choice === myPositions.c3 || 
+        Players.playerOne.choice === myPositions.b1 && Players.playerOne.choice === myPositions.b2 && Players.playerOne.choice === myPositions.b3 || 
+        Players.playerOne.choice === myPositions.c1 && Players.playerOne.choice === myPositions.c2 && Players.playerOne.choice === myPositions.c3) {
+        Players.playerOne.winner = true;
+        Game.gameOver.set(true);
+      }
+      else if(myPositions.a1 !== '' && myPositions.a2 !== '' && myPositions.a3 !== '' && myPositions.b1 !== '' 
+        && myPositions.b3 !== '' && myPositions.c1 !== '' && myPositions.c2 !== '' && myPositions.c3 !== undefined) {
+          Game.gameOver.set(true);
+      } else if(Players.playerTwo.choice === myPositions.a1 && Players.playerTwo.choice === myPositions.a2 && Players.playerTwo.choice === myPositions.a3 || 
+        Players.playerTwo.choice === myPositions.a1 && Players.playerTwo.choice === myPositions.b2 && Players.playerTwo.choice === myPositions.c3 || 
+        Players.playerTwo.choice === myPositions.a1 && Players.playerTwo.choice === myPositions.b1 && Players.playerTwo.choice === myPositions.c1 || 
+        Players.playerTwo.choice === myPositions.a2 && Players.playerTwo.choice === myPositions.b2 && Players.playerTwo.choice === myPositions.c2 || 
+        Players.playerTwo.choice === myPositions.a3 && Players.playerTwo.choice === myPositions.b2 && Players.playerTwo.choice === myPositions.c1 || 
+        Players.playerTwo.choice === myPositions.a3 && Players.playerTwo.choice === myPositions.b3 && Players.playerTwo.choice === myPositions.c3 || 
+        Players.playerTwo.choice === myPositions.b1 && Players.playerTwo.choice === myPositions.b2 && Players.playerTwo.choice === myPositions.b3 || 
+        Players.playerTwo.choice === myPositions.c1 && Players.playerTwo.choice === myPositions.c2 && Players.playerTwo.choice === myPositions.c3) {
+        Players.playerTwo.winner = true;
+        Game.gameOver.set(true);
+      }
+      // console.log(Players.playerOne.winner)
+      // console.log(Players.playerTwo.winner)
+      if(Game.gameOver.getGameOver()) {
+        Game.endGame();
+      } else {
+        Game.switchTurn();
+      }
+    }
 
   const reset = () => {
-    positions = [];
+    myPositions.reset();
     squares.forEach(square => {
       square.textContent = '';
     })
-    Game.gameOver.gameOver = true;
+    Game.gameOver.set(true);
   }
   
   const init = (() => {
-    addSubmitListener();
     addSquareListeners();
+    // myPositions();
   })();
 
   return {
     placeInPosition: placeInPosition,
-    positions : positions,
+    myPositions : myPositions,
     reset: reset,
-    addSubmitListener: addSubmitListener,
   };
 
 })();
